da6b55c48e1a408378dc4a9c62d59f30433c821d
Jerome Doran <disneyfan9184@gmail.com>
2023-05-20 14:43:31 -0400

    commented out database code

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,28 +1,28 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { signIn } from 'next-auth/react';
-import { connectDB } from '@lib/dbConnect';
+// import NextAuth from 'next-auth';
+// import GoogleProvider from 'next-auth/providers/google';
+// import { signIn } from 'next-auth/react';
+// import { connectDB } from '@lib/dbConnect';
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      await connectDB();
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   async session({ session }) {},
+//   async signIn({ profile }) {
+//     try {
+//       await connectDB();
 
-      // check if user already exists
+//       // check if user already exists
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  },
-});
+//       return true;
+//     } catch (error) {
+//       console.log(error);
+//       return false;
+//     }
+//   },
+// });
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
